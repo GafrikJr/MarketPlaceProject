a0e8ce10066829e5851b7103248c854d945f717b
GafrikJr <gafrikjr22848@gmail.com>
2025-05-16 18:06:12 +0300

    Добавил меню открытия и закрытия ПВЗ

---
+++ src/main/java/org/example/menu/sellpointManagement/OpenNewSellPoint.java
@@ -0,0 +1,34 @@
+package org.example.menu.sellpointManagement;
+
+import org.example.menu.stockManagement.StockMenu;
+import org.example.methods.otherMethods.ClearConsole;
+import org.example.methods.warehousesMethods.SellPointMethods;
+import org.example.methods.warehousesMethods.StockMethods;
+import org.example.model.warehouses.Warehouse;
+
+import java.util.Scanner;
+
+public class OpenNewSellPoint {
+    public static void openNewSellPoint() {
+        ClearConsole.clearConsole();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите зарплатный бюджет склада: ");
+        int salaryBudget = scanner.nextInt();
+        System.out.println("Введите вместимость склада: ");
+        int capacity = scanner.nextInt();
+        Warehouse warehouse = new Warehouse(salaryBudget, capacity);
+        SellPointMethods.openSellPoint(warehouse); // создали запись сток и warehouse
+
+        System.out.println("Если хотите открыть еще ПВЗ - нажмите 1" +
+                "\nЕсли хотите вернуться назад - нажмите 2");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                openNewSellPoint();
+                break;
+            case 2:
+                SellPointMenu.menu();
+                break;
+        }
+    }
+}
